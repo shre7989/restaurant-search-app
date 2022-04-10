7dc87e29631be23e728e6b2dff3d909a47b19015
Mausam Shrestha <shre7989@gmail.com>
2022-04-10 01:41:55 -0700

    init project

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({});
